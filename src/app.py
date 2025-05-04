2efcaee67afb0188cecbd02e97c6f64143dfdfca
Alex Law <alex.law@cotportal.co.uk>
2025-05-04 10:13:39 +0100

    app refactored

--- src/app.py
+++ src/app.py
@@ -1,14 +1,13 @@
-import streamlit as st
 from datetime import timedelta, time
+import streamlit as st
 
 import backend
-from config import LOW_PRICE_START_TIME, LOW_PRICE_END_TIME
+from config import LOW_PRICE_START_TIME, LOW_PRICE_DURATION, CHARGE_RATE
 from models import DemoAdminState, BatteryState, ChargerState
 from plotting import plot_upcoming_charges
 from utils import ( 
     get_current_time_to_nearest_30_minutes,
     battery_indicator,
-    get_scheduled_override,
     add_period_to_rounded_time
 )
 
@@ -23,7 +22,7 @@ def set_start_end_low_price() -> tuple[time, time]:
     else:
         low_price_start_limit = LOW_PRICE_START_TIME
     low_price_start = st.time_input('Low Price Start Time', low_price_start_limit)
-    low_price_end_limit = add_period_to_rounded_time(low_price_start, timedelta(hours=3))
+    low_price_end_limit = add_period_to_rounded_time(low_price_start, timedelta(hours=LOW_PRICE_DURATION))
     low_price_end = st.time_input('Low Price End Time', low_price_end_limit)
     st.session_state['low_price_start_limit'] = add_period_to_rounded_time(low_price_end_limit, timedelta(hours=21))
     return low_price_start, low_price_end
@@ -34,7 +33,6 @@ def get_demo_state() -> DemoAdminState:
     with st.sidebar:
         st.subheader("Demo Admin Controls")
         st.write("Use these controls to simulate the car and charger state.")
-
         percentage_input = st.number_input(
             "Battery %",
             min_value=0,
@@ -43,26 +41,43 @@ def get_demo_state() -> DemoAdminState:
             help="Enter Battery %"
         )
         soc = percentage_input / 100.0
-
         current_time = st.time_input("Current Time", rounded_time)
-        
         low_price_start, low_price_end = set_start_end_low_price()
-
-        
-
         car_is_plugged_in = st.toggle("Plugged in", value=True)
-
-    # Hardcoding charge rate as 0.1 soc / 1hr
-    charge_rate = 0.1
 
     return DemoAdminState(
         car_is_plugged_in=car_is_plugged_in,
         low_price_start=low_price_start,
         low_price_end=low_price_end,
         current_time=current_time,
-        battery_state=BatteryState(soc=soc, charge_rate=charge_rate)
+        battery_state=BatteryState(soc=soc, charge_rate=CHARGE_RATE)
     )
 
+
+def main(demo_state):
+    """
+    Main function to handle charging app
+    """
+    # Display battery percentage
+    battery_placeholder = st.empty()
+    battery_html = battery_indicator(demo_state.battery_state)
+    battery_placeholder.markdown(battery_html, unsafe_allow_html=True)
+
+    # Plot graph
+    st.subheader("Charging Schedule")
+    df_plot = backend.get_future_states(demo_state)
+    st.plotly_chart(
+        plot_upcoming_charges(
+            df_plot,
+            current_time=demo_state.current_time,
+        )
+    )
+
+    # Display control buttons
+    backend.button_control(
+        demo_state.car_is_plugged_in,
+        demo_state.battery_state.soc
+    )
 
 if __name__ == "__main__":
     # Not setting demo_state to be a session state since should only be changed during initial set up
@@ -77,26 +92,6 @@ if __name__ == "__main__":
         if not demo_state.car_is_plugged_in:
             st.session_state['charger_state'] = ChargerState(car_is_charging=False, charge_is_override=False, desired_soc=1.0)
         st.session_state['initial_load'] = False
-    # Display battery percentage
-    battery_placeholder = st.empty()
-    battery_html = battery_indicator(demo_state.battery_state)
-    battery_placeholder.markdown(battery_html, unsafe_allow_html=True)
 
-    st.subheader("Charging Schedule")
-
-    df_plot = backend.get_future_states(demo_state)
-
-    st.plotly_chart(
-        plot_upcoming_charges(
-            df_plot,
-            current_time=demo_state.current_time,
-        )
-    )
-
-    car_is_charging, charge_is_override = get_scheduled_override()
-
-
-    backend.button_control(
-        demo_state.car_is_plugged_in,
-        demo_state.battery_state.soc
-    )
+    main(demo_state)
+    
--- src/config.py
+++ src/config.py
@@ -4,6 +4,9 @@ from datetime import time, timedelta
 PERIOD = timedelta(minutes=30)
 # How many period steps to show in graph
 RANGE_STEPS = 9
-# Start and end time for low price
+# Start time for low price
 LOW_PRICE_START_TIME = time(2, 0)
-LOW_PRICE_END_TIME = time(5, 0)
+# How long should default low price be
+LOW_PRICE_DURATION = 3
+# How fast does the battery charge 0.1 soc / 1hr
+CHARGE_RATE = 0.1
